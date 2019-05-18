4bb74eb058cd831861050f1cf00de95a8a0229c5
vitaliibalakin <balakinvitalyv@gmail.com>
2019-05-18 15:08:47 +0700

    test for bpm lines finished

---
+++ corr_resp_proc.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+import pyqtgraph as pg
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout
+from PyQt5 import uic
+
+
+class BpmPreproc(QMainWindow):
+    def __init__(self):
+        super(BpmPreproc, self).__init__()
+        pg.setConfigOption('background', 'w')
+        pg.setConfigOption('foreground', 'k')
+        uic.loadUi("test.ui", self)
+        self.show()
+
+        self.cur_bpm = 'bpm01'
+        self.chan_bpm_vals = {'bpm01': {}, 'bpm02': {}, 'bpm03': {}, 'bpm04': {}, 'bpm05': {}, 'bpm07': {}, 'bpm08': {},
+                              'bpm09': {}, 'bpm10': {}, 'bpm11': {}, 'bpm12': {}, 'bpm13': {}, 'bpm14': {}, 'bpm15': {},
+                              'bpm16': {}}
+        self.bpm_val_renew = {'bpm01': 0, 'bpm02': 0, 'bpm03': 0, 'bpm04': 0, 'bpm05': 0, 'bpm07': 0, 'bpm08': 0,
+                              'bpm09': 0, 'bpm10': 0, 'bpm11': 0, 'bpm12': 0, 'bpm13': 0, 'bpm14': 0, 'bpm15': 0,
+                              'bpm16': 0}  # , 'bpm17': 0}
+        print('start')
+        self.plot = pg.PlotWidget()
+        self.plot.showGrid(x=True, y=True)
+        self.plot.setRange(yRange=[0, 400])
+        p = QVBoxLayout()
+        self.widget.setLayout(p)
+        p.addWidget(self.plot)
+
+        self.cur_lines = {'0': np.ones([16, ]), '1': np.ones([16, ]), '2': np.ones([16, ]), '3': np.ones([16, ])}
+
+        self.comboBox.currentTextChanged.connect(self.bpm_changed)
+
+    def bpm_changed(self):
+        self.cur_bpm = self.comboBox.currentText()
+
+    def data_proc(self, chan):
+        if chan.name.split('.')[-2] == self.cur_bpm:
+            self.cur_lines[chan.name.split('.')[-1][-1]] = chan.val
+            self.plot.clear()
+            self.plot.plot(self.cur_lines['0'], pen=pg.mkPen('r'))
+            self.plot.plot(self.cur_lines['1'], pen=pg.mkPen('g'))
+            self.plot.plot(self.cur_lines['2'], pen=pg.mkPen('b'))
+            self.plot.plot(self.cur_lines['3'], pen=pg.mkPen('y'))
+            self.coor_calc()
+
+
+if __name__ == "__main__":
+    app = QApplication(['BPM'])
+    w = BpmPreproc()
+    sys.exit(app.exec_())
--- resp_matrix_asseml.py
+++ resp_matrix_asseml.py
@@ -5,13 +5,13 @@ from PyQt5.QtCore import QTimer
 
 import sys
 import functools
-import pycx4.qcda as cda
+import numpy as np
 
 from basic_module import BasicFunc
 
 
 class ResponseMatrixAssembling(BasicFunc):
-    def __init__(self, corr_names=0, bpm_names=0):
+    def __init__(self, corr_names=None, bpm_names=None):
         super(ResponseMatrixAssembling, self).__init__()
         # self.corr_names = ['c1d2_z', 'c1f2_x', 'c1f1_x', 'c1d1_z', 'c2d2_z', 'c2f2_x', 'c2f1_x', 'c2d1_z', 'c3d2_z',
         #                    'c3f2_x', 'c3f1_x', 'c3d1_z', 'c4d2_z', 'c4f2_x', 'c4f1_x', 'c4d1_z', 'crm1', 'crm2', 'crm3',
@@ -25,10 +25,12 @@ class ResponseMatrixAssembling(BasicFunc):
         self.corr_err = []
         self.flag = False
         self.init_corr_values = {}
+        self.orbit_resp_data = np.zeros([15, ])
+        self.orbit_changes_data = np.zeros([15, ])
 
-        self.counter = {'c_val': -4, 'bpm': 0, 'c_name': 0}  # -4 to make symmetrical interval -4..4
-        self.stop = {'c_val': 5, 'bpm': 10, 'c_name': len(self.corr_names)}
-        self.step = 100
+        self.counter = {'c_val': -19, 'c_name': 0}  # -4 to make symmetrical interval -4..4
+        self.stop = {'c_val': 20, 'c_name': len(self.corr_names)}
+        self.step = 20
         self.c_name = self.corr_names[self.counter['c_name']]
 
         self.corr_values, self.corr_chans = self.chans_connect({'Iset': {}, 'Imes': {}}, {'Iset': {}, 'Imes': {}},
@@ -39,7 +41,7 @@ class ResponseMatrixAssembling(BasicFunc):
 
     def bpm_data_proc(self):
         # go to next step
-        print('bpm_data_proc')
+        self.orbit_resp_data = np.vstack(self.orbit_resp_data, self.bpm_values)
         self.resp_matr_ass_proc()
 
     def resp_matr_ass_proc(self):
@@ -59,9 +61,10 @@ class ResponseMatrixAssembling(BasicFunc):
             else:
                 self.corr_chans['Iset'][self.c_name].setValue(self.init_corr_values[self.c_name])
                 self.counter['c_name'] += 1
+                self.save_corr_resp()
                 if self.counter['c_name'] != self.stop['c_name']:
                     self.c_name = self.corr_names[self.counter['c_name']]
-                    self.counter['c_val'] = -4
+                    self.counter['c_val'] = -19
                     self.resp_matr_ass_proc()
                 else:
                     self.save_rma()
@@ -69,6 +72,17 @@ class ResponseMatrixAssembling(BasicFunc):
             QTimer.singleShot(3000, functools.partial(self.err_verification, self.corr_values, self.corr_err,
                                                       [ResponseMatrixAssembling, 'bpm_data_proc'],
                                                       'f_stop_chan', 'rma'))
+
+    def save_corr_resp(self):
+        for i in range(self.orbit_resp_data[:, 0] - 2):
+            self.orbit_changes_data = np.vstack(self.orbit_changes_data, self.orbit_resp_data[i + 1] -
+                                                self.orbit_resp_data[i + 2])
+            
+        np.savetxt(self.c_name + '.txt', self.orbit_changes_data,
+                   header=(self.c_name + '|' + str(self.init_corr_values['Imes'][self.c_name]) + '19'))
+
+        self.orbit_resp_data = np.zeros([15, ])
+        self.orbit_changes_data = np.zeros([15, ])
 
     def save_rma(self):
         print('rma collecting finished')

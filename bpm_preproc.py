c77d7c0af9288d0c1bd06b2fe8639ad07a645be3
vitaliibalakin <balakinvitalyv@gmail.com>
2019-06-23 18:55:46 +0700

    before monday exp ver 2

--- bpm_preproc.py
+++ bpm_preproc.py
@@ -3,17 +3,22 @@
 import numpy as np
 import pycx4.qcda as cda
 import sys
-from PyQt5.QtWidgets import QApplication
+import datetime
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
 
 
-class BpmPreproc:
+class BpmPreproc(QMainWindow):
     def __init__(self):
         super(BpmPreproc, self).__init__()
+        uic.loadUi("for_fft.ui", self)
         self.bpm_x = {}
         self.bpm_z = {}
         self.chan_bpm_marker = []
         self.chan_bpm_numpts = []
         self.chan_bpm_vals = []
+        self.for_fft_x = []
+        self.for_fft_z = []
 
         self.bpms = {'bpm01': 21.4842, 'bpm02': 23.3922, 'bpm03': 24.6282, 'bpm04': 26.5572, 'bpm05': 0.8524,
                      'bpm07': 2.7974, 'bpm08': 4.0234, 'bpm09': 5.9514, 'bpm10': 7.7664, 'bpm11': 9.6884,
@@ -26,6 +31,7 @@ class BpmPreproc:
 
         self.chan_x_orbit = cda.VChan('cxhw:4.bpm_preproc.x_orbit', max_nelems=16)
         self.chan_z_orbit = cda.VChan('cxhw:4.bpm_preproc.z_orbit', max_nelems=16)
+        self.btn_fft.clicked.connect(self.save_fft)
         print('start')
 
         for bpm, bpm_coor in self.bpm_val_renew.items():
@@ -44,6 +50,10 @@ class BpmPreproc:
             chan.valueMeasured.connect(self.bpm_marker)
             self.chan_bpm_marker.append(chan)
 
+    def save_fft(self):
+        time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        np.savetxt("for_fft" + " " + time, np.vstack((self.for_fft_x, self.for_fft_z)))
+
     def bpm_numpts(self, chan):
         self.bpm_numpts_renew[chan.name.split('.')[-2]] = chan.val
 
@@ -52,6 +62,10 @@ class BpmPreproc:
         data_len = int(self.bpm_numpts_renew[bpm_num][0])
         self.bpm_x[bpm_num] = np.mean(chan.val[data_len:2*data_len-1])
         self.bpm_z[bpm_num] = np.mean(chan.val[2*data_len:3*data_len-1])
+
+        if bpm_num == 'bpm15':
+            self.for_fft_x = chan.val[data_len:2*data_len-1]
+            self.for_fft_z = chan.val[2*data_len:3*data_len-1]
 
     def bpm_marker(self, chan):
         self.bpm_val_renew[chan.name.split('.')[-2]] = 1

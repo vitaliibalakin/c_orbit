d19d47c03af02524f7d98aea8ad4021f50198b73
vbalakin <balakinvitalyv@gmail.com>
2020-01-13 12:39:15 +0700

    created class bpm

--- bpm_preproc/bpm_preproc.py
+++ bpm_preproc/bpm_preproc.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python3
 # bpm delay is 20527.89 ns in pickup2 channel - check
 
-#import PyQt5
 import numpy as np
 import pycx4.pycda as cda
 import json
 from aux.service_daemon import CXService
+
+
+class BPM:
+    def __init__(self, bpm, receiver):
+        super(BPM, self).__init__()
+        self.receiver = receiver
+        self.name = bpm
+        self.turns_mes = 0
+        self.act_state = 1
+        self.marker = 0
+        self.coor = (0, 0)
+        self.sigma = (0, 0)
+
+        self.chan_turns = cda.VChan('cxhw:4.bpm_preproc.turns', max_nelems=131072)
+        self.chan_fft = cda.VChan('cxhw:4.bpm_preproc.fft', max_nelems=262144)
+        self.chan_datatxzi = cda.VChan('cxhw:37.ring.' + bpm + '.datatxzi', max_nelems=4096)
+        self.chan_numpts = cda.DChan('cxhw:37.ring.' + bpm + '.numpts')
+        self.chan_datatxzi.valueMeasured.connect(self.data_proc)
+
+    def data_proc(self, chan):
+        data_len = len(chan.val)
+        if self.turns_mes:
+            self.chan_fft.setValue(chan.val[data_len:3 * data_len])
+            self.chan_turns.setValue(chan.val[3*data_len:4*data_len])
+        if not self.marker:
+            self.marker = 1
+            self.coor = (np.mean(chan.val[data_len:2 * data_len]), np.mean(chan.val[2 * data_len:3 * data_len]))
+            self.sigma = (np.std(chan.val[data_len:2 * data_len]), np.std(chan.val[2 * data_len:3 * data_len]))
+            self.receiver()
 
 
 class BpmPreproc:
@@ -13,25 +41,12 @@ class BpmPreproc:
         super(BpmPreproc, self).__init__()
         self.fft_bpm = 'bpm15'
         self.turns_bpm = 'bpm15'
-        self.bpm = {}
-        self.bpm_sigma = {}
-        self.chan_bpm_numpts = {}
-        self.chan_bpm_marker = []
-        self.chan_bpm_vals = []
-        self.cur_bpm_list = ['bpm01', 'bpm02', 'bpm03', 'bpm04', 'bpm05', 'bpm07', 'bpm08', 'bpm09', 'bpm10', 'bpm11',
-                             'bpm12', 'bpm13', 'bpm14', 'bpm15', 'bpm16', 'bpm17']
 
-        self.bpms = ['bpm01', 'bpm02', 'bpm03', 'bpm04', 'bpm05', 'bpm07', 'bpm08', 'bpm09', 'bpm10', 'bpm11', 'bpm12',
-                     'bpm13', 'bpm14', 'bpm15', 'bpm16', 'bpm17']
-        self.bpm_val_renew = {'bpm01': 0, 'bpm02': 0, 'bpm03': 0, 'bpm04': 0, 'bpm05': 0, 'bpm07': 0, 'bpm08': 0,
-                              'bpm09': 0, 'bpm10': 0, 'bpm11': 0, 'bpm12': 0, 'bpm13': 0, 'bpm14': 0, 'bpm15': 0,
-                              'bpm16': 0, 'bpm17': 0}
-        self.bpm_numpts_renew = self.bpm_val_renew.copy()
-        self.bpm_val_ren_cur = {bpm: 0 for bpm in self.cur_bpm_list}
+        self.bpms_list = ['bpm01', 'bpm02', 'bpm03', 'bpm04', 'bpm05', 'bpm07', 'bpm08', 'bpm09', 'bpm10', 'bpm11',
+                          'bpm12', 'bpm13', 'bpm14', 'bpm15', 'bpm16', 'bpm17']
+        self.bpms = [BPM(bpm, self.bpm_marker) for bpm in self.bpms_list]
 
         self.chan_orbit = cda.VChan('cxhw:4.bpm_preproc.orbit', max_nelems=64)
-        self.chan_turns = cda.VChan('cxhw:4.bpm_preproc.turns', max_nelems=131072)
-        self.chan_fft = cda.VChan('cxhw:4.bpm_preproc.fft', max_nelems=262144)
         self.chan_cmd = cda.StrChan('cxhw:4.bpm_preproc.cmd', max_nelems=1024, on_update=1)
         self.chan_act_bpm = cda.StrChan('cxhw:4.bpm_preproc.act_bpm', max_nelems=1024)
         self.chan_res = cda.StrChan('cxhw:4.bpm_preproc.res', max_nelems=1024)
@@ -40,60 +55,28 @@ class BpmPreproc:
         self.chan_cmd.valueMeasured.connect(self.cmd)
         print('start')
 
-        for bpm, bpm_coor in self.bpm_val_renew.items():
-            # bpm numpts init
-            chan = cda.DChan('cxhw:37.ring.' + bpm + '.numpts')
-            chan.valueMeasured.connect(self.bpm_numpts)
-            self.chan_bpm_numpts[bpm] = chan
+    def bpm_marker(self):
+        permission = 0
+        for bpm in self.bpms:
+            if bpm.act_state:
+                if bpm.marker:
+                    permission = 1
+                else:
+                    permission = 0
+                    break
 
-            # bpm channels init
-            chan = cda.VChan('cxhw:37.ring.' + bpm + '.datatxzi', max_nelems=4096)
-            chan.valueMeasured.connect(self.data_proc)
-            self.chan_bpm_vals.append(chan)
-
-            # bpms marker init
-            chan = cda.VChan('cxhw:37.ring.' + bpm + '.marker')
-            chan.valueMeasured.connect(self.bpm_marker)
-            self.chan_bpm_marker.append(chan)
-
-    def bpm_numpts(self, chan):
-        bpm_name = chan.name.split('.')[-2]
-        self.bpm_numpts_renew[bpm_name] = chan.val
-        if bpm_name == self.turns_bpm:
-            self.chan_res.setValue(json.dumps({'num_pts': chan.val}))
-
-    def data_proc(self, chan):
-        bpm_num = chan.name.split('.')[-2]
-
-        data_len = int(self.bpm_numpts_renew[bpm_num])
-        self.bpm[bpm_num] = (np.mean(chan.val[data_len:2 * data_len]), np.mean(chan.val[2 * data_len:3 * data_len]))
-        self.bpm_sigma[bpm_num] = (np.std(chan.val[data_len:2 * data_len]), np.std(chan.val[2 * data_len:3 * data_len]))
-        if bpm_num == self.fft_bpm:
-            self.chan_fft.setValue(chan.val[data_len:3 * data_len])
-
-        if bpm_num == self.turns_bpm:
-            self.chan_turns.setValue(chan.val[3*data_len:4*data_len])
-
-        # if bpm_num == 'bpm07':
-        #     self.btn_fft.setText(str(round(chan.val[data_len], 5)))
-        #     print(chan.val[data_len + +111: data_len+114])
-
-    def bpm_marker(self, chan):
-        # print('mark')
-        self.bpm_val_ren_cur[chan.name.split('.')[-2]] = 1
-        if all(sorted(self.bpm_val_ren_cur.values())):
-            for key in self.bpm_val_renew:
-                self.bpm_val_renew[key] = 0
+        if permission:
             x_orbit = np.array([])
             x_orbit_sigma = np.array([])
             z_orbit = np.array([])
             z_orbit_sigma = np.array([])
-            for key in self.bpms:
-                if key in self.cur_bpm_list:
-                    x_orbit = np.append(x_orbit, self.bpm[key][0])
-                    x_orbit_sigma = np.append(x_orbit_sigma, self.bpm_sigma[key][0])
-                    z_orbit = np.append(z_orbit, self.bpm[key][1])
-                    z_orbit_sigma = np.append(z_orbit_sigma, self.bpm_sigma[key][1])
+            for bpm in self.bpms:
+                bpm.marker = 0
+                if bpm.act_state:
+                    x_orbit = np.append(x_orbit, bpm.coor[0])
+                    x_orbit_sigma = np.append(x_orbit_sigma, bpm.sigma[0])
+                    z_orbit = np.append(z_orbit, bpm.coor[1])
+                    z_orbit_sigma = np.append(z_orbit_sigma, bpm.coor[1])
                 else:
                     x_orbit = np.append(x_orbit, 100.0)
                     x_orbit_sigma = np.append(x_orbit_sigma, 0.0)
@@ -105,7 +88,11 @@ class BpmPreproc:
     def cmd(self, chan):
         cmd = json.loads(chan.val)
         try:
-            self.fft_bpm = self.turns_bpm = cmd['turn_bpm']
+            for bpm in self.bpms:
+                if bpm.name == cmd['turn_bpm']:
+                    bpm.turns_mes = 1
+                else:
+                    bpm.turns_mes = 0
         except KeyError:
             pass
 
@@ -116,19 +103,20 @@ class BpmPreproc:
 
     def act_bpm(self, chan):
         try:
-            act_bpm = json.loads(chan.val)
-            self.cur_bpm_list = act_bpm['cur_bpms']
+            act_bpm = json.loads(chan.val)['cur_bpms']
+            for bpm in self.bpms:
+                if bpm.name in act_bpm:
+                    bpm.act_state = 1
+                else:
+                    bpm.act_state = 0
         except Exception as exc:
             print(exc)
 
     def update_num_pts(self, num_pts):
-        self.chan_bpm_numpts[self.turns_bpm].setValue(num_pts)
+        for bpm in self.bpms:
+            if bpm.turns_mes:
+                bpm.chan_numpts.setValue(num_pts)
 
-
-# if __name__ == "__main__":
-#     app = QApplication(['bpm_preproc'])
-#     w = BpmPreproc()
-#     sys.exit(app.exec_())
 
 class KMService(CXService):
     def main(self):

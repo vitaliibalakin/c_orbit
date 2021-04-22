ff2d41269cdfdcf3d2560515d492f3ae4f6f1b20
vbalakin <balakinvitalyv@gmail.com>
2021-04-22 15:40:49 +0700

    betatron freqs val filtered

--- knobs/injection_vs_knob.py
+++ knobs/injection_vs_knob.py
@@ -38,7 +38,15 @@ class InjTune(QMainWindow):
         self.chan_tunes = cda.VChan('cxhw:4.bpm_preproc.tunes', max_nelems=2, on_update=True)
         self.chan_tunes.valueMeasured.connect(self.tunes_changed)
         self.chan_extracted = cda.DChan('cxhw:0.dcct.extractioncurrent', on_update=True)
-        self.chan_extracted.valueMeasured.connect(self.extracted_event)
+        self.chan_extracted.valueMeasured.connect(self.extracted_current)
+        self.chan_eshots = cda.DChan('cxhw:0.ddm.eshots')
+        self.chan_eshots.valueMeasured.connect(self.shots_num)
+        self.chan_eshots = cda.DChan('cxhw:0.ddm.pshots')
+        self.chan_eshots.valueMeasured.connect(self.shots_num)
+        self.chan_modet = cda.StrChan("cxhw:0.k500.modet", max_nelems=4, on_update=True)
+        self.chan_modet.valueMeasured.connect(self.modet)
+        self.chan_modet = cda.DChan("cxhw:0.ddm.extracted", on_update=True)
+        self.chan_modet.valueMeasured.connect(self.extraction)
 
         self.p_win.handles_table.cellPressed.connect(self.index)
         self.handle_1:object = BtnHandle(self.p_win.btn_handle_1, 'Handle #1', 'Handle #2')
@@ -51,16 +59,20 @@ class InjTune(QMainWindow):
         self.shift:function = None  # tune shift func
         self.handle:object = None
         ##########################
+        self.extraction_flag:bool = False
         self.cur_flag:bool = False
         self.tunes_flag:bool = False
+        self.shots_skip:dict = {'p': 10, 'e': 2}
         ##########################
         self.marked_row:int = None
         self.cross_booked:dict = {'Handle #1': None, 'Handle #2': None}
         ##########################
+        self.mode:str = 'e'
         self.cur_tunes:list = [0.0, 0.0]
         self.ring_cur_arr:list = []
         self.ring_cur_data:dict = {}
         ##########################
+        self.skip_counter:int = 0
         self.counter:int = 0
         self.n_amount:int = 36
         self.shots_counter:int = 1
@@ -250,14 +262,28 @@ class InjTune(QMainWindow):
             self.cur_1_it += 1
         QTimer.singleShot(6000, self.next_step)
 
-    def tunes_changed(self, chan) -> None:
-        if self.tunes_flag:
-            self.cur_tunes[0] = round(chan.val[0], 5)
-            self.cur_tunes[1] = round(chan.val[1], 5)
-            self.cur_flag = True
-            self.tunes_flag = False
+    def shots_num(self, chan) -> None:
+        self.shots_skip[chan.name[-6]] = chan.val // 2 + 1
 
-    def extracted_event(self, chan) -> None:
+    def modet(self, chan)-> None:
+        self.mode = chan.val[0]
+
+    def extraction(self, chan):
+        self.extraction_flag = True
+
+    def tunes_changed(self, chan) -> None:
+        if self.extraction_flag and self.tunes_flag:
+            if self.skip_counter < self.shots_skip[self.mode]:
+                self.skip_counter += 1
+            else:
+                self.skip_counter = 0
+                self.cur_tunes[0] = round(chan.val[0], 5)
+                self.cur_tunes[1] = round(chan.val[1], 5)
+                self.cur_flag = True
+                self.tunes_flag = False
+                self.extraction_flag = False
+
+    def extracted_current(self, chan) -> None:
         if self.shots_counter >= self.n_shots:
             self.shots_counter = 0
             self.ring_cur_data[self.counter] = {json.dumps(self.cur_tunes): round(np.mean(self.ring_cur_arr), 1)}
